7742cc51f18a3e6bd12103b8bb51bc817b40cd89
Yixing Huang <yxhuangfau@gmail.com>
2019-08-06 17:40:51 +0200

    add noise
    add noise

--- src/edu/stanford/rsl/cellImaging/ParallelRecon120DegreeWithNoise.java
+++ src/edu/stanford/rsl/cellImaging/ParallelRecon120DegreeWithNoise.java
@@ -16,7 +16,7 @@ import ij.ImagePlus;
  *
  */
 
-public class ParallelRecon120Degree {
+public class ParallelRecon120DegreeWithNoise {
 
 	public static void main (String [] args) throws Exception{
 		new ImageJ();
@@ -30,7 +30,7 @@ public class ParallelRecon120Degree {
 		int sizeY = sizeX;
 		int s = 2; //sampling factor
 		ImagePlus imp, impFbp, impArtifact;
-		ParallelRecon120Degree obj = new ParallelRecon120Degree();
+		ParallelRecon120DegreeWithNoise obj = new ParallelRecon120DegreeWithNoise();
 		Grid2D phan, recon, reconLimited, artifact, sinogram, filteredSinogram;
 		int numDet = 512;
 		double deltaS = 1;
@@ -38,11 +38,12 @@ public class ParallelRecon120Degree {
 		ParallelBackprojector2D backproj = new ParallelBackprojector2D(sizeX/s, sizeY/s, s, s);
 		RamLakKernel ramLak = new RamLakKernel(numDet, deltaS);
 		int idSave;
-		for(int imgIdx = 1; imgIdx <= 1; imgIdx++ )
+		for(int imgIdx = 1; imgIdx <= 94; imgIdx++ )
 		{
 			imgPath = folderPath + imgIdx + ".tif";
 			imp = IJ.openImage(imgPath);
 			phan = ImageUtil.wrapImagePlus(imp).getSubGrid(0);
+			obj.addFOVCircle(phan);
 			for(int rotIdx = 0; rotIdx < 4; rotIdx++)
 			{
 				idSave = 1000 + (imgIdx - 1) * 4 + rotIdx;
@@ -59,13 +60,16 @@ public class ParallelRecon120Degree {
 				}
 				if(imgIdx == 1 && rotIdx == 0)
 					filteredSinogram.show("The Filtered Sinogram");
-				
-
 				recon = backproj.backprojectPixelDriven(filteredSinogram);
 				if(imgIdx == 1 && rotIdx == 0)
 					recon.clone().show("recon");
 				
-				for(int j = 120; j < sinogram.getSize()[1]; j++)
+				obj.addPoissonNoise(sinogram, 1.0e6);
+				filteredSinogram = new Grid2D(sinogram);
+				for (int theta = 0; theta <= 120; ++theta) {
+					ramLak.applyToGrid(filteredSinogram.getSubGrid(theta));
+				}
+				for(int j = 121; j < sinogram.getSize()[1]; j++)
 					for(int i = 0; i < sinogram.getSize()[0]; i++)
 						filteredSinogram.setAtIndex(i, j, 0);
 				reconLimited = backproj.backprojectPixelDriven(filteredSinogram);
@@ -84,8 +88,9 @@ public class ParallelRecon120Degree {
 				IJ.saveAs(impArtifact, "Tiff", artifactPath);
 				System.out.print(imgIdx + "_" + rotIdx + " ");
 			}
-			
+			System.out.println();
 		}
+		System.out.println("Finished!");
 		
 	}
 	
@@ -101,5 +106,47 @@ public class ParallelRecon120Degree {
 		return imgRot;
 	}
 	
+	private void addPoissonNoise(Grid2D sinogram, double d) throws Exception{
+		double val;
+		float amp = 50.f;
+		sinogram.getGridOperator().divideBy(sinogram, amp);
+		Grid2D I = new Grid2D(sinogram.getWidth(), sinogram.getHeight());
+		for (int i = 0; i < sinogram.getWidth(); i ++)
+			for(int j = 0; j < sinogram.getHeight(); j++)
+			{
+				val = d * Math.pow(Math.E, -sinogram.getAtIndex(i, j));
+				I.setAtIndex(i, j, (float)(val));
+			}
+		
+
+		PoissonNoiseFilteringTool poisson = new PoissonNoiseFilteringTool();
+		poisson.applyToolToImage(I);
+	
+		
+		for (int i = 0; i < sinogram.getWidth(); i ++)
+			for(int j = 0; j < sinogram.getHeight(); j++)
+			{
+				val = - Math.log(I.getAtIndex(i, j)/d);
+				sinogram.setAtIndex(i, j, (float)(val>=0?val:0));
+			
+			}
+		sinogram.getGridOperator().multiplyBy(sinogram, amp);
+	}
+	
+	private void addFOVCircle(Grid2D phan)
+	{
+		float r = (phan.getSize()[0] - 1.0f)/2.0f;
+		float rr = r * r;
+		float xCent = r;
+		float yCent = xCent;
+		float dd;
+		for(int i = 0; i < phan.getSize()[0]; i ++)
+			for(int j = 0; j < phan.getSize()[1]; j ++)
+			{
+				dd = (i - xCent) * (i - xCent) + (j - yCent) * (j - yCent);
+				if(dd <= rr && dd > rr - 250)
+					phan.setAtIndex(i, j, 1.0f);
+			}
+	}
 	
 }
