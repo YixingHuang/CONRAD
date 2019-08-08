c89e2f5ad7d50260ca8333657928962fc006839a
Yixing Huang <yxhuangfau@gmail.com>
2019-08-08 11:36:16 +0200

    Update blue-green algae phantom
    Update blue-green algae phantom
    and
    generate phantoms

--- src/edu/stanford/rsl/cellImaging/BlueGreenAlgaePhantom.java
+++ src/edu/stanford/rsl/cellImaging/BlueGreenAlgaePhantom.java
@@ -4,28 +4,30 @@
 package edu.stanford.rsl.cellImaging;
 
 import edu.stanford.rsl.conrad.data.numeric.Grid3D;
-import edu.stanford.rsl.conrad.phantom.SheppLogan3D;
 
 /**
- * @author febmeng
+ * @author Yixing Huang
+ * this is a blue-green algae phantom. It contains a cell wall, multiple cyanosomes inside the cell wall, and multiple gold nanoparticles outside the cell wall.
+ * The sizes and positions of the cyanosomes and gold nanoparticles vary at each time, as a random function is used.
  *
  */
 public class BlueGreenAlgaePhantom {
 
-	boolean init = false;
-	int nx = 0;
-	int ny = 0;
-	int nz = 0;
-	double wx, wy, wz;
-	double dx, dy, dz;
-	Grid3D image;
-	SheppLogan3D phan;
-	int numCyano = 10;
-	double[][] ellipsoids;
+	private Grid3D image;
+	private boolean init = false;
+	private int nx = 0;
+	private int ny = 0;
+	private int nz = 0;
+	private double wx, wy, wz;
+	private double dx, dy, dz;
+	private int numCyano = 10;
+	private int numNano = 50; //number of gold nano-particles
+	private double[][] ellipsoids;
 
-	public BlueGreenAlgaePhantom( int maxI, int maxJ, int maxK, int numCyano ){
+
+	public BlueGreenAlgaePhantom( int maxI, int maxJ, int maxK, int numCyano, int numNano){
 		
-		initBlueGreenAlgae3D(maxI, maxJ, maxK, numCyano);
+		initBlueGreenAlgae3D(maxI, maxJ, maxK, numCyano, numNano);
 		
 		if (nx == 0 || ny == 0 || nz == 0 ){
 			System.out.println("Errpr: Wrong volume image size!");			
@@ -35,13 +37,13 @@ public class BlueGreenAlgaePhantom {
 	}
 	
 	
-	public BlueGreenAlgaePhantom( Grid3D grid, int numCyano ){
+	public BlueGreenAlgaePhantom( Grid3D grid, int numCyano, int numNano){
 		this.image = grid;
-		initBlueGreenAlgae3D( image.getSize()[0], image.getSize()[1], image.getSize()[2], numCyano);
+		initBlueGreenAlgae3D( image.getSize()[0], image.getSize()[1], image.getSize()[2], numCyano, numNano);
 	}
 	
 	
-	void initBlueGreenAlgae3D( int maxI, int maxJ, int maxK, int numCyano){
+	private void initBlueGreenAlgae3D( int maxI, int maxJ, int maxK, int numCyano, int numNano){
 
 		this.nx = maxI;
 		this.ny = maxJ;
@@ -53,31 +55,59 @@ public class BlueGreenAlgaePhantom {
 		dy = 2.0 / ( ny - 1 );
 		dz = 2.0 / ( nz - 1 );
 		this.numCyano = numCyano;
-		generateEllipsoids();
-		phan = new SheppLogan3D(this.ellipsoids);
+		this.numNano = numNano;
 		init = true;
 	}
 
-	void computeNumericalPhantom( ){
+	private void computeNumericalPhantom( ){
+		double x, y, z;
+
+		double maxR = 0;
+		double centX, centY, centZ;
+		int maxXYZ = Math.max(Math.max(nx, ny), nz);
 		
-		//System.out.print("nx = " + nx + ", ny = " + ny + ", nz = " + nz + " .\n");
-		//System.out.print("dx = " + dx + ", dy = " + dy + ", dz = " + dz + " .\n");
-		//System.out.print("wx = " + wx + ", wy = " + wy + ", wz = " + wz + " .\n");
+		double[][] RT; //transposed rotation matrices
 		
-		for( int ix = 0; ix < nx; ix++ ){
+		double signal;
+		double[] p;
+		double sum;
+		int iix, iiy, iiz;
+		Grid3D tag = new Grid3D(nx, ny, nz);
+		for(int idx = 0; idx < numCyano + numNano + 2; idx ++)
+		{
+			RT = transpose(dot(dot( this.Rx(ellipsoids[idx][6]), this.Ry(ellipsoids[idx][7])),this.Rz(ellipsoids[idx][8])));
 			
-			double x = ( ix - wx ) * dx ;
-			
-			for( int iy = 0; iy < ny; iy++ ){
-				
-				double y = ( iy - wy ) * dy ;
-				
-				for( int iz = 0; iz < nz; iz++ ){
-					
-					double z = ( iz - wz ) * dz ;
-					
-					image.setAtIndex(ix, iy, iz, (float)phan.ImageDomainSignal(x, y, z));
-					
+			maxR = Math.max(Math.max(ellipsoids[idx][3], ellipsoids[idx][4]), ellipsoids[idx][5]) * maxXYZ / 2.0;
+			centX = wx + nx / 2.0 * ellipsoids[idx][0];
+			centY = wy + ny / 2.0 * ellipsoids[idx][1];
+			centZ = wz + nz / 2.0 * ellipsoids[idx][2];
+			for(int ix = - (int) maxR; ix <= maxR; ix++)
+			{
+				x = ix * dx;
+				iix = ix + (int) centX;
+				if(iix < 0 || iix >= nx)
+					continue;
+				for(int iy = - (int) maxR; iy <= maxR; iy++)
+				{
+					y = iy * dy;
+					iiy = iy + (int) centY;
+					if(iiy < 0 || iiy >= ny)
+						continue;
+					for(int iz = - (int) maxR; iz <= maxR; iz++)
+					{						
+						z = iz * dz;
+						iiz = iz + (int) centZ;
+						if(iiz < 0 || iiz >= nz || tag.getAtIndex(iix, iiy, iiz) == 1)
+							continue;
+						p = dot(RT,new double[] {x, y, z});
+			            
+			            sum = Math.pow(p[0]/ellipsoids[idx][3], 2) + Math.pow(p[1]/ellipsoids[idx][4], 2) + Math.pow(p[2]/ellipsoids[idx][5], 2);
+			        
+			            signal = (sum<=1.0)?ellipsoids[idx][9]:0;
+			            image.setAtIndex(iix, iiy, iiz, image.getAtIndex(iix, iiy, iiz) + (float) signal);
+			            if(idx >= 2 && sum <= 1.0)
+			            	tag.setAtIndex(iix, iiy, iiz, 1);
+					}
 				}
 			}
 		}
@@ -86,22 +116,28 @@ public class BlueGreenAlgaePhantom {
 	
 	public Grid3D getNumericalPhantom(){
 		if( init) 
+		{
+			generateEllipsoids();
 			computeNumericalPhantom();
+		}
 		return image;
 	}
 	
+	public void resetVolume(){
+		image.getGridOperator().fill(image, 0);
+	}
 	
 	private void generateEllipsoids(){
-		ellipsoids = new double[numCyano + 2][10];
-		double r = 0.78;
-		double r2 = r + 0.02;
-		double[][] background = 	 
-			// { delta_x, delta_y, delta_z,        a,       b,       c,            phi,  theta,  psi,     rho }
-			{  {    0,       0,       0,           r2,      r2,      r2,             0,      0,    0,     1 },
-			{       0,       0,       0,           r,       r,       r,              0,      0,    0,   -0.8 }};
+		ellipsoids = new double[numCyano + numNano + 2][10];
+		double r = 0.7; //cell wall inner radius
+		double r2 = r + 0.02; //cell wall outer radius
+		double[][] cellWall =
+				// { delta_x, delta_y, delta_z,        a,       b,       c,            phi,  theta,  psi,     rho }
+				{  {    0,       0,       0,           r2,      r2,      r2,             0,      0,    0,     1 },
+				{       0,       0,       0,           r,       r,       r,              0,      0,    0,   -0.8 }};
 		
-		System.arraycopy(background[0], 0, ellipsoids[0], 0, 10);
-		System.arraycopy(background[1], 0, ellipsoids[1], 0, 10);
+		System.arraycopy(cellWall[0], 0, ellipsoids[0], 0, 10);
+		System.arraycopy(cellWall[1], 0, ellipsoids[1], 0, 10);
 		int i = 0;
 		double[][] temp = new double[1][10];
 		double r3;
@@ -118,32 +154,109 @@ public class BlueGreenAlgaePhantom {
 			temp[0][4] = r3 + (Math.random() - 0.5) * 0.005;
 			temp[0][5] = r3 + (Math.random() - 0.5) * 0.01;
 			temp[0][9] = 0.1 + (Math.random() - 0.5) * 0.1;
-			if(checkBoundary(temp, r))
+			if(checkBoundary(temp, r, 0.01))
 			{
-				System.arraycopy(temp[0], 0, ellipsoids[i+2], 0, 10);
+				System.arraycopy(temp[0], 0, ellipsoids[i + 2], 0, 10);
 				i++;
 			}
-		}	
+		}
+		
+		i = 0;
+		
+		while(i < numNano)
+		{
+			for(int j = 0; j < 3; j++)
+			{
+				temp[0][j] = (Math.random() - 0.5) * 2;
+				temp[0][j + 6] = (Math.random() - 0.5) * Math.PI; 
+			}
+			
+			r3 = 0.02 + (Math.random() - 0.5) * 0.01;
+			temp[0][3] = r3;
+			temp[0][4] = r3 + (Math.random() - 0.5) * 0.01;
+			temp[0][5] = r3 + (Math.random() - 0.5) * 0.01;
+			temp[0][9] = 2.0 + (Math.random() - 0.5) * 0.3;
+			if(checkNanoBoundary(temp, r2, 0))
+			{
+				System.arraycopy(temp[0], 0, ellipsoids[i + numCyano + 2], 0, 10);
+				i++;
+			}
+		}
 	}
 	
-	private boolean checkBoundary(double [][] ellipsoid, double r)
+	private boolean checkBoundary(double [][] ellipsoid, double r, double tolerance)
 	{
-//		for(int i = 0; i < 3; i++)
-//			if(Math.abs(ellipsoid[0][i]) + ellipsoid[0][i] >= r - 0.01)
-//			{
-//				return false;
-//			}
+
 		double dd = 0;
 		double d;
 		for(int i = 0; i < 3; i++)
 			dd += ellipsoid[0][i] * ellipsoid[0][i];
 		d = Math.sqrt(dd);
 		double maxR = Math.max(Math.max(ellipsoid[0][3], ellipsoid[0][4]), ellipsoid[0][5]);
-		if(maxR + d >= r - 0.01)
+		if(maxR + d >= r - tolerance)
 			return false;
 		else		
 			return true;
 	}
+	
+	private boolean checkNanoBoundary(double [][] ellipsoid, double r, double tolerance)
+	{
+
+		double dd = 0;
+		double d;
+		for(int i = 0; i < 3; i++)
+			dd += ellipsoid[0][i] * ellipsoid[0][i];
+		d = Math.sqrt(dd);
+		double maxR = Math.max(Math.max(ellipsoid[0][3], ellipsoid[0][4]), ellipsoid[0][5]);
+		if( d - maxR >= r + tolerance)
+			return true;
+		else		
+			return false;
+	}
+	
+//	 private static double[][] cellWall =
+//				// { delta_x, delta_y, delta_z,        a,       b,       c,            phi,  theta,  psi,     rho }
+//				{  {    0,       0,       0,           r2,      r2,      r2,             0,      0,    0,     1 },
+//				{       0,       0,       0,           r,       r,       r,              0,      0,    0,   -0.8 }};
+	 
+	 
+	 private double [] dot(double [][] one, double [] two){
+	    	double [] vector = new double [one.length];
+	    	for (int i= 0; i < one.length; i++){
+	    		double sum = 0;
+	    		for(int j=0;j < two.length; j++){
+	    			sum += one[i][j] * two[j];
+	    		}
+	    		vector[i]=sum;
+	    	}
+	    	return vector;
+	    }
+	 
+	  private double [] [] dot(double [][] one, double [][] two){
+	    	return new Jama.Matrix(one).times(new Jama.Matrix(two)).getArray();
+	    }
+	   
+     private double[][] Rx(double t){
+
+         return new double[][] {{1, 0, 0}, {0, Math.cos(t), -Math.sin(t)}, {0, Math.sin(t), Math.cos(t)}};
+         
+     }
+     
+     private double[][] Ry(double t){
+
+         return new double[][] {{Math.cos(t), 0, Math.sin(t)}, {0, 1, 0}, {-Math.sin(t), 0, Math.cos(t)}};
+         
+     }
+     
+     private double[][] Rz(double t){
+
+         return new double[][]{{Math.cos(t), -Math.sin(t), 0}, {Math.sin(t), Math.cos(t), 0}, {0, 0, 1}};
+         
+     }
+     
+     private double [] [] transpose(double [] [] in){
+     	return new Jama.Matrix(in).transpose().getArray();
+     }
 	
 }
 /*
---
+++ src/edu/stanford/rsl/cellImaging/GenerateBlueGreenAlgaePhantom.java
@@ -0,0 +1,34 @@
+package edu.stanford.rsl.cellImaging;
+
+import edu.stanford.rsl.conrad.data.numeric.Grid3D;
+import edu.stanford.rsl.conrad.utils.ImageUtil;
+import ij.IJ;
+import ij.ImageJ;
+import ij.ImagePlus;
+
+
+public class GenerateBlueGreenAlgaePhantom {
+
+	public static void main(String[] args) {
+		new ImageJ();
+		BlueGreenAlgaePhantom phanObj = new BlueGreenAlgaePhantom(512, 512, 512, 50, 100);
+		Grid3D phan;
+		
+//		phan= phanObj.getNumericalPhantom();
+//		phan.show("algae");
+		
+		ImagePlus imp;
+		String saveFolderPath = "D:\\Tasks\\FAU4\\CellImaging\\AlgaePhantoms\\";
+		String saveName;
+		for(int i = 10; i <= 10; i++)
+		{
+			phanObj.resetVolume();
+			phan = phanObj.getNumericalPhantom();
+			imp = ImageUtil.wrapGrid3D(phan, null);
+			saveName = saveFolderPath + i + ".tif";
+			IJ.saveAs(imp, "Tiff", saveName);
+			System.out.print(" " + i);
+		}
+	}
+	
+}
